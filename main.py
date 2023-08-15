d6da1dcca6ebb93c1685f83ad0767a47b23f835f
diegops17 <psdiego17@gmail.com>
2023-08-14 23:21:21 -0300

    consumindo uma api de cotação de moedas

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+import json
+import locale
+
+url = requests.get(f'https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL').json()
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+#dolar = url['USDBRL']['bid']
+print(f"Cotação do Dólar US$ {locale.currency(float(url['USDBRL']['bid']))}")
+print(f"Cotação do Euro € {locale.currency(float(url['EURBRL']['bid']))}")
+print(f"Cotação do Bitcoin ₿ {locale.currency(float(url['BTCBRL']['bid']))}")
